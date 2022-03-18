1af88a7b059b8e54428cba01fc80282805c38281
raghuth <raghavendrath21@gmail.com>
2022-03-18 16:27:30 +0530

    Add files via upload
    Added task-samples-day-2.js file changes

---
+++ task-samples-day-2.js
@@ -0,0 +1,80 @@
+  
+ // JavaScript basics:
+ 
+ // Thorough the JavaScript if else 2.https://www.youtube.com/watch?v=IsG4Xd6LlsM video link 
+
+
+ //  2. JavaScript if else (tutorial)
+ //  1.if and else
+    
+ // Execute the below codes
+ 
+ ///// Sample 1 ///////
+  
+console.log('---------------  sample 1 ---------------');
+  
+let hour = 20
+
+if (hour >=6 && hour <12) {
+  console.log("Good morning")
+} else if (hour >=12 && hour < 18) {
+  console.log("Good afternoon") 
+} else { 
+  console.log("Good evening") 
+}
+
+console.log('---------------  sample 1 ---------------');
+
+///// Sample 2 ///////
+
+// check if the number is positive, negative or zero
+
+//const  number = prompt("Enter a number: ");
+
+console.log('---------------  sample 2 ---------------');
+
+let number = 0
+if (number >= 0) {
+    if (number == 0) {
+        console.log("You entered number 0");
+    } else {
+        console.log("You entered a positive number");
+    }
+} else {
+    console.log("You entered a negative number");
+}
+
+console.log('---------------  sample 2 ---------------');
+
+///// Sample 3 ///////
+
+// find equal  the number  or not 
+
+console.log('---------------  sample 3 ---------------');
+const  getNumber = 10
+
+if (getNumber ===10) {
+   console.log("Yes number is 10");
+} else if (getNumber === 20) {
+   console.log("Yes number is 20");
+} else {
+   console.log("You entered a different number");
+}
+console.log('---------------  sample 3 ---------------');
+
+///// Sample 4 ///////
+
+// check the value is empty
+
+//const  getNumber = prompt("Enter a number: ");
+console.log('---------------  sample 4 ---------------');
+
+if (getNumber =='') {
+   console.log("input value is empty");
+} else{
+    console.log("input value is not empty");
+ }
+ console.log('---------------  sample 4 ---------------');
+ 
+ //////////////
+
