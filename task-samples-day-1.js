3feb00f6a4a7a0d36a97fd52f34c9cae0a6d9dea
raghuth <raghavendrath21@gmail.com>
2022-03-18 17:00:18 +0530

    Add files via upload
    Added task-samples-day-1.js file changes

---
+++ task-samples-day-1.js
@@ -0,0 +1,43 @@
+console.log('Thorough the What is JavaScript? 1. https://www.youtube.com/watch?v=upDLs1sn7g4 video link')
+console.log(`What is JavaScript?
+what can you do with it?
+where does javascript code run?
+javascript vs ECMAScript?`)
+
+console.log('----------------- 1.What is Javascript? -----------------------')
+
+console.log(`  Answer : 
+ er: a, JavaScript is the Programming Language for the Web.
+
+     i. b. JavaScript can update and change both HTML and CSS.
+
+    ii. c. provides interactivity
+
+   iii. d. provides real-time content up updates.
+
+   And did some examples:
+
+I run the javascript code in the browser console and execute the below code
+ 
+1.console.log('hello word')
+2. 2+2 4
+3. alert('hello world')`)
+
+
+console.log('----------------- 2.What can you do with it? -----------------------')
+
+  console.log(`Answer : a programming language run in your browser provides interactivity provides real-time content up updates
+every browser is a javascript engine we can run javascript code like chrome, firefox browsers`)
+
+
+console.log('----------------- 3.where does javascript code run? -----------------------')
+
+console.log(`Answer : javascript run only browser every browser like a javascript engine like chrome, firefox(browser and node provide runtime environment)`)
+
+console.log('----------------- 4.javascript vs ECMAScript? -----------------------')
+
+console.log(`Answer : ECMAScript - It is the scripting language that formed the basis of browser-based Javascript
+ECMA(European computer manufactures association) is a private international nonprofit standard organization 
+Javascript is the implementation of ECMAScript.`)
+
+//////////////
